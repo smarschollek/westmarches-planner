4c3a31ef8a49add970a42523b0328b75e1f632f6
Stephan Marschollek <stephan.marschollek@gmail.com>
2022-02-26 22:31:40 +0100

    adds api for uploading images

---
+++ pages/api/images/upload.tsx
@@ -0,0 +1,27 @@
+import { IncomingMessage } from 'http';
+import multer from 'multer';
+import { NextApiRequest, NextApiResponse } from 'next';
+import nextConnect from 'next-connect';
+
+type IncomingFileMessage = IncomingMessage & {
+    file: Express.Multer.File
+}
+
+const upload = multer({dest: 'public/uploads/'});
+
+const apiRoute = nextConnect();
+
+apiRoute.use(upload.single('upload'));
+
+apiRoute.post((req : IncomingFileMessage, res : any) => {
+	res.status(200).json({ data: 'success' , guid: req.file.filename });
+});
+
+export default apiRoute;
+
+export const config = {
+	api: {
+		bodyParser: false,
+		sizeLimit: '1mb'
+	},
+};
