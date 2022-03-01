2aa5d4e2f7498b48668443786b9e9bcab25bf20f
Stephan Marschollek <stephan.marschollek@gmail.com>
2022-03-01 19:30:12 +0100

    adds feature to remove quest from subscribedQuests in user

--- pages/api/quests/unsubscribe.ts
+++ pages/api/quests/unsubscribe.ts
@@ -4,6 +4,8 @@ import { apiProtector } from '../../../helper/api-protector';
 import { dbConnect } from '../../../helper/db-connect';
 import { Quest, QuestModel } from '../../../models/quest-model';
 import {Document} from 'mongoose';
+import { validateSession } from '../../../helper/validate-session';
+import { User, UserModel } from '../../../models/user-model';
 
 interface UnsubscribeQuestRequest {
     subscriberId: string
@@ -19,6 +21,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => apiProtecto
 
 const protectedHandler = async (req: NextApiRequest, res: NextApiResponse) => {
 	try {
+		const session = await validateSession(req);
 		schema.validate(req.body);
 		const request = req.body as UnsubscribeQuestRequest;
     
@@ -32,8 +35,16 @@ const protectedHandler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     	quest.subscriber.splice(index, 1);
     	await quest.save();
+
+			const user = await UserModel.findById(session.id);
+			if(user) {
+				const index = user.subscribedQuests.indexOf(request.questId);
+				if(index !== -1) {
+					user.subscribedQuests.splice(index, 1);
+					user.save();
+				}
+			}
 		}
-    
 		res.status(200).send('');
 	} catch (error) {
 		res.status(500).send('');
