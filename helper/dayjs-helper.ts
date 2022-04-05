83db629fd24bc76bbff4d92b3d0ef7f6b57f179f
Stephan Marschollek <stephan.marschollek@gmail.com>
2022-04-05 22:10:16 +0200

    removes unused functions from dayjs-helper

--- helper/dayjs-helper.ts
+++ helper/dayjs-helper.ts
@@ -1,14 +1,7 @@
 import dayjs, { Dayjs } from 'dayjs';
-import isoWeeksInYear from 'dayjs/plugin/isoWeeksInYear.js';
-import isLeapYear from 'dayjs/plugin/isLeapYear.js';
 import isoWeek from 'dayjs/plugin/isoWeek.js';
-import dayOfYear from 'dayjs/plugin/DayOfYear.js';
 
-
-dayjs.extend(isoWeeksInYear);
-dayjs.extend(isLeapYear);
 dayjs.extend(isoWeek);
-dayjs.extend(dayOfYear);
 
 export interface CalenderWeek {
     weekIndex: number,
@@ -41,40 +34,6 @@ const convertDaysJsToWeekDay = (value: Dayjs) : WeekDay => {
 		num: value.format('DD'),
 		date: value.valueOf()
 	};
-};
-
-export const getCalenderWeeks = () : CalenderWeek[] => {
-	const result : CalenderWeek[] = [];
-
-	for (let i = 0; i <= dayjs().isoWeeksInYear(); i++) {
-		const week = dayjs().isoWeek(i);
-
-		result.push({
-			weekIndex: i, 
-			name: 'KW' + (i+1),
-			start: dayjs(week).isoWeekday(1).toDate(),
-			stop: dayjs(week).isoWeekday(7).toDate()
-		});
-	}
-
-	return result;
-};
-
-export const getWeekDays = (weekIndex : number) : Date[] => {
-	const result : Date[] = [];
-	const week = dayjs().isoWeek(weekIndex);
-	for (let i = 1; i < 8; i++) {
-		const day = dayjs(week).isoWeekday(i).toDate();
-		day.setHours(23);
-		day.setMinutes(59);
-		day.setSeconds(59);
-		result.push(day);  
-	}
-	return result;
-};
-
-export const getCurrentWeek = () : number => {
-	return dayjs().isoWeek();
 };
 
 export const groupTimes = (times: number[]) : number[][] => {
